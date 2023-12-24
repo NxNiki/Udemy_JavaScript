80f3dbd0ef19d7c71a83dcaa379d954f98f44086
Xin Niu <xinniu@Xins-MacBook-Air.local>
2023-12-24 01:39:49 -0600

    module2.1

--- module2.1_scope_and_closure/module2.1_objects_contest.js
+++ module2.1_scope_and_closure/module2.1_objects_contest.js
@@ -3,4 +3,6 @@ a_variable.a_property = { b : 2 };
 
 console.log(a_variable);
 
-{ c : 3 }.a_property = { b : 2 };
+// { c : 3 }.a_property = { b : 2 }; // this won't work, the brace is interpreted as the opening of a block.
+
+console.log(({ c : 3 }).a_property = { b : 2 });
